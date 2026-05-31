056ce0733cecdcc08ad6404ca325ef9b9274d4d1
Coto Cisternas <coto@petabyte.cl>
2026-05-30 21:07:53 -0400

    test(scrapctl): cover CLI failure paths

--- cmd/scrapctl/main.go
+++ cmd/scrapctl/main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/petabytecl/scrap/internal/scrapctl"
 )
 
 func main() {
-	if err := scrapctl.Run(os.Args[1:], os.Stdout, os.Stderr, scrapctl.Deps{}); err != nil {
-		fmt.Fprintf(os.Stderr, "scrapctl: %v\n", err)
-		os.Exit(1)
+	os.Exit(run(os.Args[1:], os.Stdout, os.Stderr))
+}
+
+func run(args []string, stdout, stderr io.Writer) int {
+	if err := scrapctl.Run(args, stdout, stderr, scrapctl.Deps{}); err != nil {
+		_, _ = fmt.Fprintf(stderr, "scrapctl: %v\n", err)
+		return 1
 	}
+	return 0
 }
